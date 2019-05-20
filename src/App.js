e0da38c0d431d0f78c0a6702edf06148a6291c87
Akataya <infotechpro@yahoo.com>
2019-05-20 18:03:06 -0400

    Added vanue info and highlights

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import "./resources/styles.css";
 
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured";
+import VenueInfo from "./components/venueinfo";
+import HighLight from "./components/Highlights";
 
 class App extends Component {
   render() {
     return (
-      <div
-        className="App"
-        style={{ height: "1500px", background: "cornflowerblue" }}
-      >
+      <div className="App" style={{ height: "1500px" }}>
         <Header />
         <Featured />
+        <VenueInfo />
+        <HighLight />
       </div>
     );
   }
---
+++ src/components/Highlights/Discount.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide";
+
+class Discount extends Component {
+  state = {
+    discountStart: 0,
+    discountEnd: 30
+  };
+
+  porcentage = () => {
+    if (this.state.discountStart < this.state.discountEnd) {
+      this.setState({
+        discountStart: this.state.discountStart + 1
+      });
+    }
+  };
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.porcentage();
+    }, 30);
+  }
+
+  render() {
+    return (
+      <div className="center_wrapper">
+        <div className="discount_wrapper">
+          <Fade onReveal={() => this.porcentage()}>
+            <div className="discount_porcentage">
+              <span> {this.state.discountStart}% </span>
+              <span> OFF </span>
+            </div>
+          </Fade>
+
+          <Slide right>
+            <div className="discount_description">
+              <h3>Purchase tickets before 20th of June!</h3>
+              <p>
+                Many desktop publishing packages and web page editors now use
+                Lorem Ipsum as their default model text, and a search for 'lorem
+                ipsum' will uncover many web sites still in their infancy.{" "}
+              </p>
+
+              <div>button</div>
+            </div>
+          </Slide>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Discount;
